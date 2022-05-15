20beecf89d687e8d44c93f7f15bf55e6cf6e525a
Solen <solen.iyassu@Ouagadougou.local>
2022-05-14 21:43:24 -0600

    added questions in array

--- script2121.js
+++ script2121.js
@@ -9,21 +9,40 @@ WHEN all questions are answered or the timer reaches 0
 THEN the game is over
 WHEN the game is over
 THEN I can save my initials and my score */
-var startButton = document.queryselector ('#start')
-var question1 = [
-    "What does CSS stand for?",
-   "
+var start = document.querySelector ('#start')
+var input = document.querySelector("#answer2")
+var nextQuestion = document.querySelector("#next")
+start.addEventListerr(click,function(){
+    startButton = 'display: none;'
+    console.log("working")
+})
+var question = [
+    {
+        Question1:"What does CSS stand for?",
+        choices: ["customer service and support", "College Scholarship Service Profile", "combat service support", "Cascading style sheet!"],
+        Answer: 3
+    },
+    {
+        Question2:"What language uses Appendchild?",
+        choices: ["JavaScript", "HTML", "jQuery", "SQL"],
+    },
+    {  
+        Question3: "Css is combined by using, ____. ",
+        choices: ["Link", "script","input", "document"],
+        Answer: 0
+    },
+    {
+        Question4: "HTML is used for _____?",
+        Answer: 0
+    },
+    {
+        Question5: "What language makes a page dynamic and functional?",
+        choices:["HTML", "SQL","Python", "Javascript"],
+        Answer: 3
+    }      
 ]
 
-var answerOptions =[
-    "Cascading style sheet!",
-    "The language is Javascript",
-    "<Link src = ...href= >",
-    "Hyper text markup Language",
-    "JavaScript!"
-]
-
-
+startButton.addEventLister('click, function()')
 
     // document.getElementById("#start").onclick = () {
     //     this.disabled = true;
@@ -31,7 +50,8 @@ var answerOptions =[
 
 
 /* var timeInterval = setInterval(function () {
-    timeLeft--;pw
+    timeLeft--;
+    timeEl.textContent = timeLeft + 'seconds left'
    
     if (secondsLeft === 0){
       clearInterval(timeInterval);
